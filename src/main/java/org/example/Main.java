2b08b9f49103f969d02e20e851710eb959bb3539
GondiMartin <martin.gondocs@gmail.com>
2023-10-19 13:06:20 +0200

    Modify structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.model.Book;
+import org.example.model.Newspaper;
+import org.example.model.Page;
+
+public class Main {
+    public static void main(String[] args) {
+        MethodTests tests = new MethodTests();
+        tests.TestBookMethod();
+        tests.TestNewspaperMethod();
+    }
+}
--- src/main/java/org/example/MethodTests.java
+++ src/main/java/org/example/MethodTests.java
@@ -1,8 +1,8 @@
+package org.example;
 import org.example.model.Book;
 import org.example.model.Newspaper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MethodTests {
 
@@ -10,13 +10,13 @@ public class MethodTests {
     void TestBookMethod(){
         Book book = new Book("Harry Potter");
         book.setTitle("The Hunger Games");
-        assertEquals(book.getTitle(), "The Hunger Games");
+        Assertions.assertEquals(book.getTitle(), "The Hunger Games");
     }
 
     @Test
     void TestNewspaperMethod(){
         Newspaper newspaper = new Newspaper("New York Time");
         newspaper.setCurrentPage(2);
-        assertEquals(newspaper.getCurrentPage(), 2);
+        Assertions.assertEquals(newspaper.getCurrentPage(), 2);
     }
 }
