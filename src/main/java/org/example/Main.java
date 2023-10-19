9b7cb4db515d29f1ac05d00eb1ebbeafef388c4e
GondiMartin <martin.gondocs@gmail.com>
2023-10-19 13:29:52 +0200

    Remove import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.example.model.Book;
 import org.example.model.Newspaper;
-import org.example.model.Page;
 
 public class Main {
     public static void main(String[] args) {
