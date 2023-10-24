19d3a191299d1dba99963a56975c50a48398e13c
GondiMartin <martin.gondocs@gmail.com>
2023-10-24 08:50:25 +0200

    Add jacoco (fail)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,6 @@
 package org.example;
 
-import org.example.model.Book;
-import org.example.model.Newspaper;
-
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Harry Potter");
-        book.setTitle("The Hunger Games");
-        System.out.println(book.getTitle());
-        Newspaper newspaper = new Newspaper("New York Time");
-        newspaper.setCurrentPage(2);
-        System.out.println(newspaper.getCurrentPage());
     }
 }
---
+++ src/test/java/org/example/MethodTests.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.model.Book;
+import org.example.model.Newspaper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MethodTests {
+
+    @Test void testBook(){
+        Book book = new Book("Harry Potter");
+        book.setTitle("The Hunger Games");
+        assertEquals("The Hunger Games", book.getTitle());
+    }
+
+    @Test void testNewspaper(){
+        Newspaper newspaper = new Newspaper("New York Time");
+        newspaper.setCurrentPage(2);
+        assertEquals(2, newspaper.getCurrentPage());
+    }
+}
